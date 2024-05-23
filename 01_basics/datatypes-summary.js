acb13359bc5adf6a6531e80c0d32665c14db48fb
disha84 <zeelpatel2831@gmail.com>
2024-05-23 17:06:21 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,9 @@
+//primitive (call by value)
+//7 types = string,number,boolean,null,undefined,symbol,bigInt(syntificvalue or big num)
+
+
+
+//reference type - non primirite 
+
+//arrays,objects,functions
+
