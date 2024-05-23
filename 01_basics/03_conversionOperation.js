d0510dd0d2b2af0ed293ce0e0c11523b4857034d
disha84 <zeelpatel2831@gmail.com>
2024-05-23 16:43:26 +0000

    conversionoperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,34 @@ console.log(booleanisloggedin);
 let someNumber = 33
 let StringNumber = String(someNumber);
 console.log( typeof StringNumber);
+
+
+//  *****************************operation******************************** // 
+
+let value =3
+let negvalue =-value
+console.log(negvalue);
+
+
+//arithmetic operation
+
+let str1 = "disha" 
+let str2 = "hello" 
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter =100
+gamecounter ++;
+console.log(gamecounter);
