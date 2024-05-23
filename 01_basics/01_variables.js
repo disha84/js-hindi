737a37c5523ca50b0ae215fc8204f3341080ed32
disha84 <zeelpatel2831@gmail.com>
2024-05-23 15:45:44 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 1234  // value cannot be change
+let accountemail= "zeel2345@gmail.com"   //value can be change
+var accountpassword = "1234"
+//in javascript to declare variable use let and var but mostly use let keyword
+
+
+accountcity ="vadodara"
+
+
+//accountID =234
+
+accountemail = "qwe@.com"
+accountpassword ="123"
+accountcity ="ahemdabad"
+console.log(accountID);
+
+/*
+prefer not to use var buz issue in function scope 
+*/
+
+console.table([accountID,accountemail,accountpassword,accountcity]);
