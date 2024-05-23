60876772262b614c42f0259e2f2911310dc75159
disha84 <zeelpatel2831@gmail.com>
2024-05-23 15:57:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //use all js code as newer version
+
+//alert("3+3") // we are using node js not browser
+
+console.log(3+3);
+
+
+let name ="disha" //string
+let age = 20 //number
+let isloggedin = false // boolean
+
+//number = range  2 to the power 53
+//bigint use for big no 
+//string can use in ""''
+//boolean  true false 
+//null stand alone value 
+//undefined when variable define but value not assign 
+//symbol  for unique value 
+
+
+console.log(typeof null); //objrct
+console.log(typeof undefine);//undefine 
