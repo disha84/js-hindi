d36c5d3457a846d329747b0c13c78b2d1f7f9815
disha84 <zeelpatel2831@gmail.com>
2024-05-23 16:12:34 +0000

    conversionoperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "disha"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+
+//"33" = 33
+//"33aaa" ="nan"
+// true = 1; false =0
+
+let isloggedin ="disha"
+
+let booleanisloggedin = Boolean(isloggedin);
+console.log(booleanisloggedin);
+
+//1=true,0=false
+//"" = false 
+// "disha" = true
+
+let someNumber = 33
+let StringNumber = String(someNumber);
+console.log( typeof StringNumber);
